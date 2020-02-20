a4cbffb25d39329a6ee166389d79a07c689ec474
Suspicious <alex@kaucher.com>
2020-02-19 20:31:01 -0700

    got rid of node_modules

--- schema.sql
+++ schema.sql
@@ -2,27 +2,30 @@ DROP DATABASE pointNclick if EXISTS;
 
 CREATE DATABASE pointNclick;
 
-CREATE TABLE users(
-    ID
-    USER_NAME
-    characterName
-    password
-    currentroom
-)
 
-CREATE TABLE items(
-    id
-    NAMES
-    IMAGE
-)
+-- use pointNclick;
+-- CREATE TABLE test(
+--     ID int not null auto_increment,
+--     email varchar (100),
+--     USER_NAME varchar (30),
+--     password varchar (30),
+--     currentroom varchar (30),
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE inventory(
-    id
-    user FK
-    item FK
-)
+-- CREATE TABLE items(
+--     id int not null auto_increment,
+--     NAMES VARCHAR (30),
+--     IMAGE varchar (100),
+-- );
 
-CREATE TABLE rooms(
-    IDENTITY
-    NAME
-)
+-- CREATE TABLE inventory(
+--     id
+--     user FK
+--     item FK
+-- )
+
+-- CREATE TABLE rooms(
+--     IDENTITY
+--     NAME
+-- )
