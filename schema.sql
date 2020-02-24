4da703825f055d5e973d1a94e27c036d63b401fe
Harley <Zonkeystripes@hotmail.com>
2020-02-23 20:08:54 -0700

    schema and seeds files

--- schema.sql
+++ schema.sql
@@ -1,24 +1,24 @@
-DROP DATABASE pointNclick if EXISTS;
+-- DROP DATABASE pointNclick if EXISTS;
 
-CREATE DATABASE pointNclick;
+-- CREATE DATABASE pointNclick;
 
 
-use pointNclick;
+-- use pointNclick;
 
-CREATE TABLE test(
-    ID int not null auto_increment,
-    email varchar (100),
-    USER_NAME varchar (30),
-    password varchar (30),
-    currentroom varchar (30),
-    PRIMARY KEY (id)
-);
+-- CREATE TABLE test(
+--     ID int not null auto_increment,
+--     email varchar (100),
+--     USER_NAME varchar (30),
+--     password varchar (30),
+--     currentroom varchar (30),
+--     PRIMARY KEY (id)
+-- );
 
-CREATE TABLE items(
-    id int not null auto_increment,
-    NAMES VARCHAR (30),
-    IMAGE varchar (100),
-);
+-- CREATE TABLE items(
+--     id int not null auto_increment,
+--     NAMES VARCHAR (30),
+--     IMAGE varchar (100),
+-- );
 
 -- CREATE TABLE inventory(
 --     id
@@ -35,3 +35,39 @@ CREATE TABLE items(
 --     IDENTITY
 --     NAME
 -- )
+
+
+DROP DATABASE if EXISTS deathClause;
+CREATE DATABASE deathClause;
+use deathClause;
+
+CREATE TABLE users(
+    UserID int not null auto_increment PRIMARY KEY,
+    User varchar (30),
+    password varchar (30)
+);
+
+CREATE TABLE gameObjects (
+    GobID int not null auto_increment PRIMARY KEY,
+    name varchar(100),
+    description varchar (100)
+);
+
+CREATE TABLE dialog (
+    DialogID int not null auto_increment PRIMARY KEY,
+    npc varchar(100),
+    part integer(20),
+    dialect varchar(255)
+);
+
+CREATE TABLE inventory (
+    InvID int not null auto_increment PRIMARY KEY,
+    FOREIGN KEY (GobID) REFERENCES gameObjects(GobID),
+    FOREIGN KEY (UserID) REFERENCES users(UserID)
+);
+
+CREATE TABLE composite (
+    MainID int not null auto_increment PRIMARY KEY,
+    FOREIGN KEY (DialogID) REFERENCES dialog(DialogID),
+    FOREIGN KEY (GobID) REFERENCES gameObjects(GobID)
+);
--- seeds.sql
+++ seeds.sql
@@ -1,38 +1,71 @@
 
-use pointNclick;
+-- use pointNclick;
 
-INSERT into avatars (name, motivation, image)
-VALUES ("Fiance", "Affair with brother", "#");
+-- INSERT into avatars (name, motivation, image)
+-- VALUES ("Fiance", "Affair with brother", "#");
 
-INSERT into avatars (name, motivation, image)
-VALUES ("Brother", "Jealous of wealthy brother", "#");
+-- INSERT into avatars (name, motivation, image)
+-- VALUES ("Brother", "Jealous of wealthy brother", "#");
 
-INSERT into avatars (name, motivation, image)
-VALUES ("One Eyed Coworker, "Has eyepatch", "#");
+-- INSERT into avatars (name, motivation, image)
+-- VALUES ("One Eyed Coworker, "Has eyepatch", "#");
 
-INSERT into avatars (name, motivation, image)
-VALUES ("Fiance", "Affair with brother", "#");
+-- INSERT into avatars (name, motivation, image)
+-- VALUES ("Fiance", "Affair with brother", "#");
 
 
--- Items for the Table
+-- -- Items for the Table
 
-INSERT INTO items (id, name, image)
-VALUES ("#", "Hair Dryer", "image");
+-- INSERT INTO items (id, name, image)
+-- VALUES ("#", "Hair Dryer", "image");
 
-INSERT INTO items (id, name, image)
-VALUES ("#", "Code Paper", "image");
+-- INSERT INTO items (id, name, image)
+-- VALUES ("#", "Code Paper", "image");
 
-INSERT INTO items (id, name, image)
-VALUES ("#", "Closet Key", "image");
+-- INSERT INTO items (id, name, image)
+-- VALUES ("#", "Closet Key", "image");
 
-INSERT INTO items (id, name, image)
-VALUES ("#", "Frozen Key", "image");
+-- INSERT INTO items (id, name, image)
+-- VALUES ("#", "Frozen Key", "image");
 
-INSERT INTO items (id, name, image)
-VALUES ("#", "Eye Drops", "image");
+-- INSERT INTO items (id, name, image)
+-- VALUES ("#", "Eye Drops", "image");
 
-INSERT INTO items (id, name, image)
-VALUES ("#", "Poison Eye Drops", "image");
+-- INSERT INTO items (id, name, image)
+-- VALUES ("#", "Poison Eye Drops", "image");
 
-INSERT INTO items (id, name, image)
-VALUES ("#", "Syringe", "image");
+-- INSERT INTO items (id, name, image)
+-- VALUES ("#", "Syringe", "image");
+
+
+INSERT INTO gameObjects (name, description) VALUES ("Penelope", "Fiance");
+INSERT INTO gameObjects (name, description) VALUES ("Bank Frankard", "Older Brother");
+INSERT INTO gameObjects (name, description) VALUES ("James Hooker", "One Eyed Coworker");
+INSERT INTO gameObjects (name, description) VALUES ("Titto", "Titto the Cat");
+INSERT INTO gameObjects (name, description) VALUES ("Frank Frankard", "Deceased Host");
+INSERT INTO gameObjects (name, description) VALUES ("Robert Plant", "They named their plant Robert.... plant");
+INSERT INTO gameObjects (name, description) VALUES ("Closet", "A door to a closet room.");
+INSERT INTO gameObjects (name, description) VALUES ("Fridge", "Where we stored our drinks for the party.");
+
+
+INSERT INTO gameObjects (name, description) VALUES ("Paper Code", "Appears to be a code to unlock something");
+INSERT INTO gameObjects (name, description) VALUES ("Ice Cube Key", "A key frozen inside an ice cube");
+INSERT INTO gameObjects (name, description) VALUES ("Hair Dryer", "A hot hair dryer");
+INSERT INTO gameObjects (name, description) VALUES ("Key", "A key to unlock something");
+INSERT INTO gameObjects (name, description) VALUES ("Poison", "A poisonous substance in a bottle");
+INSERT INTO gameObjects (name, description) VALUES ("Eyedrops", "Looks like it's a medicinal type of eye drop");
+
+INSERT INTO dialog (npc, part, dialect) VALUES ("Penelope", 1, "I can't believe he is dead!! *Sobs*");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Bank Frankard", 1, "He actually bought a security code to get ice out of the fridge? Waste of money.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("James Hooker", 1, "I just couldn't see this happening.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Titto", 1, "Meow!!");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Frank Frankard", 1, "This just happened, eyes crossed and everything. How could this have happened?");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Robert Plant", 1, "Looks good, smells nice.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Closet", 1, "Locked, needs a key.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Fridge", 1, "Needs a code to get ice. Weird.");
+
+
+
+
+
+
