ff50ea1fdda3b27e1d54d776f55422060d4a9cd2
Harley <Zonkeystripes@hotmail.com>
2020-02-26 20:33:28 -0700

    Dialog seeds finished.

--- schema.sql
+++ schema.sql
@@ -1,42 +1,3 @@
--- DROP DATABASE pointNclick if EXISTS;
-
--- CREATE DATABASE pointNclick;
-
-
--- use pointNclick;
-
--- CREATE TABLE test(
---     ID int not null auto_increment,
---     email varchar (100),
---     USER_NAME varchar (30),
---     password varchar (30),
---     currentroom varchar (30),
---     PRIMARY KEY (id)
--- );
-
--- CREATE TABLE items(
---     id int not null auto_increment,
---     NAMES VARCHAR (30),
---     IMAGE varchar (100),
--- );
-
--- CREATE TABLE inventory(
---     id
---     user FK
---     item FK
--- )
-
--- CREATE TABLE rooms(
---     IDENTITY
---     NAME
--- )
-
--- CREATE TABLE rooms(
---     IDENTITY
---     NAME
--- )
-
-
 DROP DATABASE if EXISTS deathClause;
 CREATE DATABASE deathClause;
 use deathClause;
@@ -68,7 +29,7 @@ CREATE TABLE inventory (
 );
 
 CREATE TABLE composite (
-    MainID int not null auto_increment PRIMARY KEY,
+    MainID int auto_increment PRIMARY KEY,
     FOREIGN KEY (DialogID) REFERENCES dialog(DialogID),
     FOREIGN KEY (GobID) REFERENCES gameObjects(GobID)
 );
--- seeds.sql
+++ seeds.sql
@@ -1,43 +1,3 @@
-
--- use pointNclick;
-
--- INSERT into avatars (name, motivation)
--- VALUES ("Fiance", "Affair with brother", "#");
-
--- INSERT into avatars (name, motivation, image)
--- VALUES ("Brother", "Jealous of wealthy brother", "#");
-
--- INSERT into avatars (name, motivation, image)
--- VALUES ("One Eyed Coworker, "Has eyepatch", "#");
-
--- INSERT into avatars (name, motivation, image)
--- VALUES ("Fiance", "Affair with brother", "#");
-
-
--- -- Items for the Table
-
--- INSERT INTO items (id, name, image)
--- VALUES ("#", "Hair Dryer", "image");
-
--- INSERT INTO items (id, name, image)
--- VALUES ("#", "Code Paper", "image");
-
--- INSERT INTO items (id, name, image)
--- VALUES ("#", "Closet Key", "image");
-
--- INSERT INTO items (id, name, image)
--- VALUES ("#", "Frozen Key", "image");
-
--- INSERT INTO items (id, name, image)
--- VALUES ("#", "Eye Drops", "image");
-
--- INSERT INTO items (id, name, image)
--- VALUES ("#", "Poison Eye Drops", "image");
-
--- INSERT INTO items (id, name, image)
--- VALUES ("#", "Syringe", "image");
-
-
 INSERT INTO gameObjects (name, description) VALUES ("Penelope", "Fiance");
 INSERT INTO gameObjects (name, description) VALUES ("Bank_Frankard", "Older Brother");
 INSERT INTO gameObjects (name, description) VALUES ("James_Hooker", "One Eyed Coworker");
@@ -47,10 +7,9 @@ INSERT INTO gameObjects (name, description) VALUES ("Plant", "They named their p
 INSERT INTO gameObjects (name, description) VALUES ("Closet", "A door to a closet room.");
 INSERT INTO gameObjects (name, description) VALUES ("Fridge", "Where we stored our drinks for the party.");
 
-
 INSERT INTO gameObjects (name, description) VALUES ("Paper_Code", "Appears to be a code to unlock something");
 INSERT INTO gameObjects (name, description) VALUES ("Ice_Cube_Key", "A key frozen inside an ice cube");
-INSERT INTO gameObjects (name, description) VALUES ("Hair_Dryer", "A hot hair dryer");
+-- INSERT INTO gameObjects (name, description) VALUES ("Hair_Dryer", "A hot hair dryer");
 INSERT INTO gameObjects (name, description) VALUES ("Key", "A key to unlock something");
 INSERT INTO gameObjects (name, description) VALUES ("Poison", "A poisonous substance in a bottle");
 INSERT INTO gameObjects (name, description) VALUES ("Eyedrops", "Looks like it's a medicinal type of eye drop");
@@ -59,9 +18,60 @@ INSERT INTO dialog (npc, part, dialect) VALUES ("Penelope", 1, "I can't believe 
 INSERT INTO dialog (npc, part, dialect) VALUES ("Bank_Frankard", 1, "He actually bought a security code to get ice out of the fridge? Waste of money.");
 INSERT INTO dialog (npc, part, dialect) VALUES ("James_Hooker", 1, "I just couldn't see this happening.");
 INSERT INTO dialog (npc, part, dialect) VALUES ("Titto", 1, "Meow?");
-INSERT INTO dialog (npc, part, dialect) VALUES ("Frank_Frankard", 1, "This just happened, eyes crossed and everything. How could this have happened?");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Frank_Frankard", 1, "It looks like something is hanging out his pocket.")
 INSERT INTO dialog (npc, part, dialect) VALUES ("Plant", 1, "Looks good, smells nice.");
 INSERT INTO dialog (npc, part, dialect) VALUES ("Closet", 1, "Locked, needs a key.");
 INSERT INTO dialog (npc, part, dialect) VALUES ("Fridge", 1, "Needs a code to get ice. Weird.");
+-- Then gets paper code, needs to click on fridge for key.
 
-INSERT INTO dialog (npc, part, dialect VALUES) ();
+INSERT INTO dialog (npc, part, dialect) VALUES ("Penelope", 2, "If I knew this would have happened, I would have killed him!");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Bank_Frankard", 2, "We should have just done this at my house, but he has more money.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("James_Hooker", 2, "Did you see that? I didn't.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Titto", 2, "Meeeeow! *Reaches paw at you*");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Frank_Frankard", 2, "I hope he doesn't start smelling the place up.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Plant", 2, "I shall call you Robert... Plant.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Closet", 2, "Still locked, needs a key.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Fridge", 2, "I wonder if that code will work.");
+-- Gets frozen key, needs to find way to defrost.
+
+INSERT INTO dialog (npc, part, dialect) VALUES ("Penelope", 3, "Even the plant is sad he is dead!");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Bank_Frankard", 3, "Do you need help with that key? I have a hair dryer.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("James_Hooker", 3, "I left my coat in the closet, didn't realize it was locked when I shut it.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Titto", 3, "*Reaches for food* Meow!!");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Frank_Frankard", 3, "Is that smell him?");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Plant", 3, "Penelope is right, it is looking kind of sad.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Closet", 3, "I bet that key goes to this...");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Fridge", 3, "I already got what I needed here.");
+--Melts ice on key, obtain key, needs to unlock closet.
+
+INSERT INTO dialog (npc, part, dialect) VALUES ("Penelope", 4, "Atleast I don't have to hear him snore anymore... wait, what are you doing??");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Bank_Frankard", 4, "I should probably call the police, but this episode is really good.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("James_Hooker", 4, "Whats brown round, lays on the ground and has wheels?");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Titto", 4, "*Plays with small toy*");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Frank_Frankard", 4, "Didn't even get to finish the episode.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Plant", 4, "You ok Robert?");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Closet", 4, "Hey this key works!");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Fridge", 4, "A code for ice? That's cold...");
+-- Door opens, coat is hanging. Click on coat, get eyedrops.
+
+INSERT INTO dialog (npc, part, dialect) VALUES ("Penelope", 5, "Oh hey, I forgot Game of Thrones is on.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Bank_Frankard", 5, "I need another drink.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("James_Hooker", 5, "That plant is in season to loose its leaves..");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Titto", 5, "*Titto ignores you*");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Frank_Frankard", 5, "Okay he smells.. Why hasn't anyone called the police yet?");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Plant", 5, "Oh no, he really doesn't look good.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Closet", 5, "");
+-- INSERT INTO dialog (npc, part, dialect) VALUES ("Coat", 5, "Hey, there are some eyedrops in here, I think they are for One Eye.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Fridge", 5, "What else will come out of this thing?");
+-- Get eyedrops, needs to find poison within plant pot.
+
+INSERT INTO dialog (npc, part, dialect) VALUES ("Penelope", 6, "Wow this episode is great! Don't you think?");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Bank_Frankard", 6, "This episode really is something.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("James_Hooker", 6, "Too bad we are out of drinks, the show isn't even over yet.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Titto", 6, "Mergaow!");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Frank_Frankard", 6, "Missed the season permire.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Plant", 6, "What is that in the pot?");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Closet", 6, "");
+-- INSERT INTO dialog (npc, part, dialect) VALUES ("Coat", 6, "It is a nice coat though.");
+INSERT INTO dialog (npc, part, dialect) VALUES ("Fridge", 6, "I already got what I needed here.");
+-- Finds poison eye drops
