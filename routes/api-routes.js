dd5d359396b48b303127e6b33d73232e65527eee
Nellie <fabybacenelson@gmail.com>
2020-02-28 23:31:12 -0700

    made inventory save

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,39 +3,39 @@ var db = require("../models");
 var passport = require("../config/passport");
 // const { Op } = require("sequelize");
 
-module.exports = function(app) {
+module.exports = function (app) {
 
   // Using the passport.authenticate middleware with our local strategy.
   // If the user has valid login credentials, send them to the members page.
   // Otherwise the user will be sent an error
-  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  app.post("/api/login", passport.authenticate("local"), function (req, res) {
     // Sending back a password, even a hashed password, isn't a good idea
     res.json({
       Username: req.user.Username,
       id: req.user.id
     });
   });
- 
-  
-  app.post("/api/register",function(req, res) {
+
+
+  app.post("/api/register", function (req, res) {
     console.log(req.body);
     db.User.create({
-      Username: req.body.Username,
-      password: req.body.password
-    })
-      .then(function() {
+        Username: req.body.Username,
+        password: req.body.password
+      })
+      .then(function () {
         console.log("success");
         res.redirect("/");
 
       })
-      .catch(function(err) {
+      .catch(function (err) {
         console.log(err);
         res.status(401).json(err);
       });
   });
 
   // Route for logging user out
-  app.get("/logout", function(req, res) {
+  app.get("/logout", function (req, res) {
     req.logout();
     res.redirect("/");
   });
@@ -52,18 +52,25 @@ module.exports = function(app) {
   //   res.redirect(307, "/api/game");
   // });
 
-
-  app.get("/api/click", function(req, res) {
+  app.put("/api/state", function (req, res) {
     if (!req.user) {
       // The user is not logged in, send back an empty object
       res.json({});
     } else {
-      // Otherwise send back the user's email and id
-      // Sending back a password, even a hashed password, isn't a good idea
-      res.json({
-        email: req.user.email,
-        id: req.user.id
-      });
+      console.log(req.body);
+      console.log(req.user.Username);
+      db.User.update({
+        inventory: req.body.inv,
+        act: req.body.act
+      }, {
+        where: {
+          Username: req.user.Username
+        }
+      }).then(function (result) {
+        res.end();
+      }).catch(function (err) {
+        console.log(err);
+      })
     }
   });
 
